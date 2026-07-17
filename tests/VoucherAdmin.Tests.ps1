1beb93c81f57f6a3e6360bc53a1ba7203b3d77fa
urboiflex <jovan.rc1212@gmail.com>
2026-07-17 16:56:19 +0800

    feat: add voucher management form

--- tests/VoucherAdmin.Tests.ps1
+++ tests/VoucherAdmin.Tests.ps1
@@ -3,6 +3,7 @@ $master = Get-Content "$root\admin_page\admin.Master" -Raw
 $masterCode = Get-Content "$root\admin_page\admin.Master.cs" -Raw
 $list = Get-Content "$root\admin_page\onyx_admin_promos.aspx" -Raw
 $listCode = Get-Content "$root\admin_page\onyx_admin_promos.aspx.cs" -Raw
+$form = if (Test-Path "$root\admin_page\onyx_admin_voucher_form.aspx") { Get-Content "$root\admin_page\onyx_admin_voucher_form.aspx" -Raw } else { '' }
 
 $checks = [ordered]@{
     'Sidebar exposes Loyalty' = $master -match '>\s*Loyalty\s*</a>'
@@ -11,6 +12,13 @@ $checks = [ordered]@{
     'List supports server actions' = $list -match 'OnItemCommand="rptVouchers_ItemCommand"' -and $listCode -match 'rptVouchers_ItemCommand'
     'List uses ONYX monochrome theme' = $list -notmatch 'bootstrap' -and $list -notmatch '#00ff87|#a78bfa'
     'List has keyboard focus styles' = $list -match '\.primary-action:focus-visible' -and $list -match '\.actions (a|\.link-button):focus-visible'
+    'Dedicated voucher form exists' = $form -match 'Voucher Details'
+    'Voucher form has identity fields' = $form -match 'ID="txtName"' -and $form -match 'ID="txtCode"'
+    'Voucher form has discount fields' = $form -match 'ID="ddlDiscountType"' -and $form -match 'ID="txtDiscountValue"' -and $form -match 'ID="txtMaximumDiscount"' -and $form -match 'ID="txtMinimumPurchase"'
+    'Voucher form has category selection' = $form -match 'ID="chkAllCategories"' -and $form -match 'ID="cblCategories"'
+    'Voucher form has validity and limits' = $form -match 'ID="txtValidFrom"' -and $form -match 'ID="txtExpiresAt"' -and $form -match 'ID="txtTotalLimit"' -and $form -match 'ID="txtPerUserLimit"'
+    'Voucher form stores plain text terms' = $form -match 'ID="txtTerms"' -and $form -notmatch 'contenteditable|innerHTML'
+    'Voucher form uses sticky action bar' = $form -match 'data-voucher-actions' -and $form -match 'position:\s*sticky'
 }
 
 $failures = @($checks.GetEnumerator() | Where-Object { -not $_.Value })
